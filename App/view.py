82653bc2885580c049eb075db1da2f46e8a364e9
scc4080 <s.cabrerar@uniandes.edu.co>
2026-08-20 09:24:44 -0500

    cambios del Estudiante 2

--- App/view.py
+++ App/view.py
@@ -55,9 +55,7 @@ def print_menu():
     print("Opciones:")
     print("1- Cargar Libros")
     print("2- Cargar Tags")
-    # TODO: Mods de Est-1, Est-2 y Est-3 en el Lab 2
-    # Agregue la opción 3 para cargar los tags de los libros.
-    # Pueede guiarse de las opciones 1 y 2.
+    print("3- Cargar BookTags")
     print("0- Salir")
 
 
@@ -108,11 +106,11 @@ def first_book(app):
 
 
 def last_book(app):
-    # TODO: Mods de Est-1 y Est-2, Est-3 en el Lab 2
     """
     Devuelve el último libro cargado en el conjunto de libros
     """
-    pass
+    last = logic.last_book(app)
+    return last
 
 
 # Se crea el controlador asociado a la vista
@@ -140,8 +138,8 @@ def main():
             # TODO: Mods de Est-1 en el Lab 2
             first = None
 
-            # TODO: Mods de Est-2 en el Lab 2
-            last = None
+            last = last_book(app)
+            print("Ultimo libro cargado: \n" + str(last) + "\n")
 
         elif int(inputs[0]) == 2:
             print("Cargando información de tags....")
