d91c95cff6cf4247f071ba870c5b68298cea89b4
LeonardoGit <gomezcifuentesleonardo@hmail.com>
2026-08-20 22:31:39 -0500

    git commit -m “Entrega Final - laboratorio2”

--- App/view.py
+++ App/view.py
@@ -88,7 +88,7 @@ def load_tags(app):
 def load_books_tags(app):
     """
     Función que carga los tags de los libros en la aplicación.
-    Carga los tags de los libros desde el archivo book_tags.csv y los almacena en la aplicación
+    Carga los tags de los libros desde el archivo book_tags-small.csv y los almacena en la aplicación
 
     :param app: Aplicación de la lógica
     :type app: logic
